5ae0fa5d256b437bab46272e4f181bee4a9b3667
Karsten A. M. Guenther <karsten.guenther@kamg.de>
2024-01-12 01:15:16 +0100

    Add tasks to run local webserver

---
+++ install.ps1
@@ -0,0 +1,18 @@
+## start of script
+# Always set the $InformationPreference variable to "Continue" globally,
+# this way it gets printed on execution and continues execution afterwards.
+$InformationPreference = "Continue"
+
+# Stop on first error
+$ErrorActionPreference = "Stop"
+
+Push-Location $PSScriptRoot
+
+try {
+   scoop install nodejs
+   npm install reveal.js
+}
+finally {
+   Pop-Location
+}
+## end of script
